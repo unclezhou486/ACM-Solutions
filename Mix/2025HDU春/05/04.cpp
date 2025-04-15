d9b0a2976e45065aec0fd7f87c9e5babe57cbca4
unclezhou486 <1543130456@qq.com>
2025-04-15 12:05:09 +0800

    2025-04-15

--- Mix/2025HDU春/05/04.cpp
+++ Mix/2025HDU春/05/04.cpp
@@ -1,169 +1,61 @@
-#include <bits/stdc++.h>
-#define int long long
+#include<bits/stdc++.h>
 using namespace std;
-
-using ll = long long;
-constexpr ll N=210;
 
 constexpr int mod=998244353;
 
-struct Matrix{
-	ll n,m;
-	ll a[N][N];
-
-	void set(ll _a,ll _b){
-		n=_a,m=_b;
-	}
-
-	Matrix(){
-		clear();
-	}
-
-	void clear(){
-		n=m=0;
-		memset(a,0,sizeof (a));
-	}
-
-	Matrix operator+(const Matrix &b) const {
-		Matrix tmp;
-		tmp.clear();
-		tmp.n=n;
-		tmp.m=m;
-		for(ll i=0;i<n;++i){
-			for(ll j=0;j<n;j++){
-				tmp.a[i][j]=a[i][j]+b.a[i][j];
+struct node{
+	vector<int> a;
+	// vector<int> b;
+	int n;
+	node():n(0){}
+	node(int x) : a(vector<int>(x)),n(x) {}
+	node operator*(const node &b) const{
+		// vector<int> c(n+10,0);
+		node c(n);
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				(c.a[(i+j)%n]+=(1ll*a[i]*b.a[j])%mod)%=mod;
 			}
 		}
-		return tmp;
+		return c;
 	}
-
-	Matrix operator*(const Matrix &b) const {
-		Matrix tmp;
-		tmp.clear();
-		tmp.n=n;
-		tmp.m=b.m;
-		for(ll i=0;i<n;++i){
-			for(ll k=0;k<m;++k){
-				if(a[i][k]==0) continue;
-				for(ll j=0;j<b.m;++j){
-					tmp.a[i][j]+=(a[i][k]*b.a[k][j])%mod;
-					tmp.a[i][j]%=mod;
-				}
-			}
-		}
-		return tmp;
-	}
-
-	Matrix get(ll x){
-		Matrix E;
-		E.clear();
-		E.set(n,m);
-		for(ll i=0;i<n;++i){
-			E.a[i][i]=1;
-		}
-		while(x){	
-			if(x&1){
-				E=*this*E;
-			}
-			*this=*this**this;
-			x>>=1;
-		}
-		return E;
-		// if(x==0) return E;
-		// else if(x==1) return *this;
-		// Matrix tmp = get(x/2);
-		// tmp = tmp*tmp;
-		// if(x*2) tmp=tmp*(*this);
-		// return tmp;
-	}
-
 };
+
+node qmi(node a,long long b){
+	node res(a.n);
+	res.a[0]=1;
+	while(b){
+		if(b&1) res=res*a;
+		a=a*a;
+		b>>=1;
+	}
+	return res;
+}
 
 void solve(){
 	long long n;
 	int m;
 	cin>>n>>m;
+	vector<int> a(m);
 	long long res=n/m;
-	vector<long long> a(m+10,res);
-	vector<long long> dp(m+10),dpp(m+10);
-	dp[0]=1;
-	Matrix C;
-	C.clear();
-	C.set(m,m);
-	for(ll i=0;i<m;i++){
-		C.a[i][i]=1;
-	}
+	node ans(m);
+	ans.a[0]=1;
+	node ress(m);
+    ress.a[0]=1;
 	for(int i=0;i<m;i++){
-		Matrix B;
-		B.clear();
-		B.set(m,m);
-		for(int j=0;j<m;j++){
-			B.a[j][j]=1;
-			B.a[(j-i+m)%m][j]+=1;
-		}
-		C=C*B;
+		node b(m);
+		b.a[0]=1;
+		b.a[i]+=1;
+		ress=ress*b;
 	}
-	Matrix A;
-	A.clear();
-	A.set(1,m);
-	A.a[0][0]=1;
-	C=C.get(res);
-	A=A*C;
-	C.clear();
-	C.set(m,m);
-	for(ll i=0;i<m;i++){
-		C.a[i][i]=1;
+	ans=ans*qmi(ress,res);
+	for(int i=1;i<=n-res*m;i++){
+		node b(m);
+		b.a[0]=1;
+		b.a[i]+=1;
+		ans=ans*b;
 	}
-	for(int i=0;i<n%m;i++){
-		// a[i]+=1;
-		Matrix B;
-		B.clear();
-		B.set(m,m);
-		for(int j=0;j<m;j++){
-			B.a[j][j]=1;
-			B.a[(j-i+m)%m][j]+=1;
-		}
-		C=C*B;
-	}
-	A=A*C;
-	// for(int i=0;i<m;i++){
-	// 	Matrix B;
-	// 	B.clear();
-	// 	B.set(m,m);
-	// 	for(int j=0;j<m;j++){
-	// 		B.a[j][j]=1;
-	// 		B.a[(j-i+m)%m][j]+=1;
-	// 	}
-	// 	// A=A*B.get(a[i]);
-	// }
-
-	cout<<(A.a[0][0]-1+mod)%mod<<'\n';
-	// auto sol = [&](long long val) -> void{
-	// 	for(int i=0;i<m;i++){
-	// 		dpp[(i+val)%m]=dp[i]+dp[(i+val)%m];
-	// 		dpp[(i+val)%m]%=mod;
-	// 	}
-	// 	for(int i=0;i<m;i++){
-	// 		dp[i]=dpp[i];
-	// 		dpp[i]=0;
-	// 	}
-	// };
-	// for(int i=0;i<m;i++){
-	// 	long long x=a[i];
-	// 	long long y=1;
-	// 	while(x>=y){
-	// 		sol(i*y);
-	// 		x-=y;
-	// 		y<<=1;
-	// 	}
-	// 	if(x){
-	// 		sol(i*x);
-	// 	}
-	// }
-	// cout<<dp[0]-1<<'\n';
-	// // for(int i=0;i<m;i++){
-	// // 	cout<<a[i]<<" \n"[i==m-1];
-	// // }
+	cout<<(ans.a[0]-1+mod)%mod<<'\n';
 }
 
 signed main(){
