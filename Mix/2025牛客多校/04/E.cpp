5490087a8d00b1f259791d63655bc6de5c611aac
unclezhou486 <1543130456@qq.com>
2025-07-27 00:14:33 +0800

    2025-07-27

--- Mix/2025牛客多校/04/E.cpp
+++ Mix/2025牛客多校/04/E.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
+constexpr int INF=1e9;
+
 struct node{
 	int pre=0,suf=0;
-	vector<int> vec1={};
+	string vec1;
 	string vec2;
 	bool flag=false;
 	node operator+(const node &b) const{
@@ -35,10 +37,10 @@ struct node{
 					return c;
 				}
 				c=*this;
-				c.vec1.push_back(suf+b.pre);
 				c.suf=b.suf;
-				for(auto x:b.vec1) c.vec1.push_back(x);
-				for(auto x:b.vec2) c.vec2.push_back(x);
+				c.vec1.push_back(min(suf+b.pre,24));
+				c.vec1+=b.vec1;
+				c.vec2+=b.vec2;
 			}
 			return c;
 		}
@@ -54,7 +56,7 @@ node newnode(int k){
 	node res;
 	if(k==1){
 		res.pre=res.suf=1;
-		res.vec1={};
+		res.vec1="";
 		res.vec2="";
 		res.flag=false;
 	}
@@ -70,19 +72,37 @@ node newnode(int k){
 	return res;
 }
 
+node newnode1(int num){
+	node res;
+	res.pre=res.suf=num;
+	res.vec1="";
+	res.vec2="";
+	res.flag=false;
+	return res;
+}
+
 struct segmentTree{
 	int n;
-	vector<node> num;
+	vector<int> num;
+	vector<int> aa;
 	void init(int n){
 		this->n=n;
-		num.assign(n<<2,{});
+		num.assign(n<<2,0);
+		aa=num;
 	}
 	void pushup(int rt){
-		num[rt]=(num[rt<<1]+num[rt<<1|1]);
+		num[rt]=max(num[rt<<1],num[rt<<1|1]);
 	}
 	void modify(int rt,int x,int l,int r,int k){
 		if(l==r){
-			num[rt]=newnode(k);
+			if(k>1){
+				num[rt]=l;
+				aa[rt]=k;
+			}
+			else{
+				num[rt]=0;
+				aa[rt]=0;
+			}
 			return;
 		}
 		int mid=(l+r)>>1;
@@ -93,26 +113,43 @@ struct segmentTree{
 	void modify(int x,int k){
 		modify(1,x,1,n,k);
 	}
-	node query(int rt,int x,int y,int l,int r){
-		if(x<=l&&r<=y){
-			return num[rt];
+	pair<int,int> find(int rt,int x,int y,int l,int r){
+		if(num[rt]<x) return {0,0};
+		if(l==r){
+			if(x<=l&&l<=y) return {l,aa[rt]};
+			else return {0,0};
 		}
-		int mid=(l+r)>>1;
-		node res;
-		if(x<=mid) res=query(rt<<1,x,y,l,mid);
-		if(mid<y) res=res+query(rt<<1|1,x,y,mid+1,r);
-		return res;
+		int mid=l+r>>1;
+		if(num[rt<<1]>=x){
+			return find(rt<<1,x,y,l,mid);
+		}
+		else{
+			return find(rt<<1|1,x,y,mid+1,r);
+		}
+	}
+	pair<int,int> find(int x,int y){
+		return find(1,x,y,1,n);
 	}
 	node query(int x,int y){
-		return query(1,x,y,1,n);
+		node res;
+		while(res.flag==false&&x<=y){
+			auto [id,val]=find(x,y);
+			if(!val||id>y){
+				res=res+newnode1(y-x+1);
+				return res;
+			}
+			res=res+newnode1(id-x)+newnode(val);
+            x=id+1;
+		}
+		return res;
 	}
 }seg;
 
 void solve(){
 	int n,q;
 	cin>>n>>q;
-	vector<int> a(n+10);
-	vector<vector<int> > adj(n+10);
+	vector<int> a(n+1);
+	vector<vector<int> > adj(n+1);
 	for(int i=1;i<=n;i++){
 		cin>>a[i];
 	}
@@ -122,7 +159,7 @@ void solve(){
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
-	vector<int> dep(n+10),f(n+10),top(n+10),dfn(n+10),siz(n+10),son(n+10,-1);
+	vector<int> dep(n+1),f(n+1),top(n+1),dfn(n+1),siz(n+1),son(n+1,-1);
 	int tot=0;
 	auto dfs1=[&](auto self,int x,int fa) -> void{
 		dep[x]=dep[fa]+1;
@@ -146,13 +183,14 @@ void solve(){
 			self(self,y,y);
 		}
 	};
-	auto lca=[&](int x,int y) -> pair<node,node>{
+	auto lca=[&](int x,int y) -> pair<node,node> {
 		// if(top[x]==top[y])
 		node resx,resy;
 		while(x!=y){
 			if(resx.flag|resy.flag) break;
 			if(top[x]==top[y]){
 				if(dep[x]>dep[y]){
+					// resx=seg.query(dfn[y]+1,dfn[x])+resx;
 					resx=seg.query(dfn[y]+1,dfn[x])+resx;
 					x=y;
 				}
@@ -173,6 +211,7 @@ void solve(){
 			}
 		}
 		resy=newnode(a[x])+resy;
+		resx.rev();
 		return {resx,resy};
 	};
 	dfs1(dfs1,1,1);
@@ -201,7 +240,6 @@ void solve(){
 				continue;
 			}
 			auto [resx,resy]=lca(l,r);
-			resx.rev();
 			node res=resx+resy;
 			if(res.flag){
 				cout<<0<<'\n';
@@ -229,16 +267,12 @@ void solve(){
 						dp|=(dp<<1);
 					}
 					int num=vec2[i];
-					// bitset<25> dpp;
 					for(int i=24;i>=0;i--){
 						int res1=0,res2=0;
 						if(i%num==0) res1=dp[i/num];
 						if(i-num>=0) res2=dp[i-num];
-						// if(i%num==0){
 						dp[i]=res1|res2;
-						// }
 					}
-					// dp=dpp;
 				}
 				for(int j=1;j<=min(vec1.back(),24);j++){
 					dp|=dp<<1;
